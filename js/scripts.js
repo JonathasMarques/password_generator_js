32e0c4371ee5363493118e124b906705f49d5926
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2023-12-19 13:00:03 -0300

    gerador de senha

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-console.log('cu')
+// seleção de elemnetos 
+
+const generatePasswordButton = document.querySelector("#open-generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+// funções
+
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.!@#$%&*+-"
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+console.log(getSymbol());
+// eventos
+
+generatePasswordButton.addEventListener("click", () => {
+    console.log("teste")
+})
